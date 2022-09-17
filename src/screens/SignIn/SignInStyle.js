3ca2f20ff87c1b2e74db7d0537d94c208933e93f
batuctn <batuctnn14@gmail.com>
2022-09-17 08:13:56 +0300

    styles file add screens

---
+++ src/screens/SignIn/SignInStyle.js
@@ -0,0 +1,7 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  safearea: { flex: 1, backgroundColor: '#111', alignItems: 'center', justifyContent: 'center' },
+  maintext: { color: 'white', fontSize: 30, marginTop: 30, fontWeight: '800', textAlign: 'center' },
+  text: { color: 'white', marginTop: 20, fontWeight: 'bold' },
+});
